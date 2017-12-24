744dc7dcc36972251613e7e7ab2aa01b5fa641ae
nqztv <nqztvme@gmail.com>
2017-12-24 03:08:09 -0700

    Update saboteur-tracker.js

--- saboteur-tracker.js
+++ saboteur-tracker.js
@@ -1,1 +1,10 @@
-
+window.onload = function() {
+  var cards = document.getElementsByClassName("card");
+  
+  var decrementCounter = 
+  
+  Array.from(cards).forEach(function(element) {
+    element.addEventListener("click", function(event) {
+      event.target.textContent = event.target.textContent - 1;
+    }, false);
+};
